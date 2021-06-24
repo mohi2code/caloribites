ed4b380c770f6e2176cb0912db00c036e002c156
mohi2code <mohyaldeen0101ahmed@gmail.com>
2021-06-24 20:08:50 +0300

    :sparkles: Goal screen

--- src/components/ActivityLevel.js
+++ src/components/ActivityLevel.js
@@ -1,10 +1,42 @@
 import { useEffect } from "react"
+import SelectCards from "./SelectCards"
+
+const values = [
+    {
+        title: "Low",
+        description: "Sitting all day, barely moving"
+    },
+    {
+        title: "Medium",
+        description: "Moderate activity through the day, light exercise"
+    },
+    {
+        title: "High",
+        description: "Active, exercises 4-5 times a week"
+    },
+    {
+        title: "Very High",
+        description: "Highly Active, spends most of the day on your feet, exercise 5-6 times a week"
+    },
+]
 
 export default function ActivityLevel({ setPage }) {
 
     useEffect(() => setPage(1), [setPage])
 
     return (
-        <h1>ActivityLevel</h1>
+        <form>
+            <div className="form-header">
+                <h1>Activity Level</h1>
+                <p>how active are you</p>
+            </div>
+
+            <div className="input-group">
+                <SelectCards
+                    values={values}
+                    onChange={selected => selected}
+                />
+            </div>
+        </form>
     )
 }
--- src/components/Goal.js
+++ src/components/Goal.js
@@ -1,10 +1,29 @@
 import { useEffect } from "react"
+import SelectCards from "./SelectCards"
+
+const values = [
+    { title: 'Lose Weight' },
+    { title: 'Maintain Weight' },
+    { title: 'Gain Weight' },
+]
 
 export default function Goal({ setPage }) {
 
     useEffect(() => setPage(2), [setPage])
 
     return (
-        <h1>Goal</h1>
+        <form>
+            <div className="form-header">
+                <h1>Goal</h1>
+                <p>what are you trying to accomplish</p>
+            </div>
+
+            <div className="input-group">
+                <SelectCards
+                    values={values}
+                    onChange={selected => selected}
+                />
+            </div>
+        </form>
     )
 }
