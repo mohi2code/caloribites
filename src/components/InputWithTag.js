74b9e3ce34df5a1ab3d0440c6b29b23c4a876668
mohi2code <55515180+mohi2code@users.noreply.github.com>
2021-06-27 00:27:41 +0300

    Revert "V3"

--- src/components/InputWithTag.js
+++
@@ -1,8 +0,0 @@
-export default function InputWithTag({ value, onChange, type, placeholder, tag }) {
-    return (
-        <div className="input-with-tag">
-            <input type={type} placeholder={placeholder} value={value} onChange={onChange} />
-            <span className="tag">{tag}</span>
-        </div>
-    )
-}
--- src/components/Pagination.js
+++
@@ -1,9 +0,0 @@
-export default function Pagination({ page }) {
-    return (
-        <div className="pagination">
-            <div className={`pagination-dot ${ page === 0 ? "pagination-selected": "" }`}></div>
-            <div className={`pagination-dot ${ page === 1 ? "pagination-selected": "" }`}></div>
-            <div className={`pagination-dot ${ page === 2 ? "pagination-selected": "" }`}></div>
-        </div>
-    )
-}
