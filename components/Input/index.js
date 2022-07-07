7c03423672a6296d90bbbad472db55f3518f3d65
MohiEldin Ahmed <mohi2code@MohiEldins-MacBook-Air.local>
2022-07-07 10:39:42 +0200

    :sparkles: Input fields

---
+++ components/Input/index.js
@@ -0,0 +1,35 @@
+import { forwardRef } from 'react'
+import classNames from 'classnames'
+import styles from './Input.module.css'
+
+const Input = forwardRef(({ size="md", children, className, ...props }, ref) => (
+    <input 
+        ref={ref}
+        className={classNames(styles['input'], className)} 
+        {...props} 
+    />
+))
+Input.displayName="Input"
+
+const Tag = ({ children, className, ...props }) => (
+    <span 
+        className={classNames(styles['tag'], className)}
+        {...props}
+    >
+        {children}
+    </span>
+)
+Tag.displayName="Tag"
+
+const InputTag = forwardRef(({ children, className, ...props }, ref) => (
+    <div 
+        ref={ref}
+        className={classNames(styles['input-tag'], className)} 
+        {...props} 
+    >
+        {children}
+    </div>
+))
+InputTag.displayName="Input"
+
+export { Input, InputTag, Tag }
--- pages/calculator.js
+++ pages/calculator.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Calculator.module.css'
 import Button from '../components/Button'
+import { Input, InputTag, Tag } from '../components/Input'
 
 export default function Calculator() {
     return (
@@ -11,13 +12,22 @@ export default function Calculator() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <div>
-                <h1>Components</h1>
+            <h1>Components</h1>
 
+            <div>
                 <Button>Primary</Button>
                 <Button type="white">White</Button>
                 <Button type="gray">Gray</Button>
             </div>
+            <div style={{width: '600px'}}>
+                <Input type="number"/>
+                <Input type="text"/>
+                <InputTag>
+                    <Input type="number"/>
+                    <Tag>KG</Tag>
+                </InputTag>
+            </div>
+
         </div>
     )
 }
