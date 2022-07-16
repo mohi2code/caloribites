2444f63649071df51f22b70b4dc5f498051ba986
MohiEldin Ahmed <mohi2code@MohiEldins-MacBook-Air.local>
2022-07-16 17:05:48 +0200

    :rocket: main app setup

--- pages/calculator.js
+++ pages/calculator.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Calculator.module.css'
 import Button from '../components/Button'
 import { Card, Header, Info } from '../components/Card'
@@ -13,37 +14,22 @@ export default function Calculator() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <h1>Components</h1>
+            <nav>
+                <Image
+                    src="/favicon.svg"
+                    alt="Logo"
+                    width={44}
+                    height={44}
+                    />
+            </nav>
 
-            <div>
-                <Button>Primary</Button>
-                <Button type="white">White</Button>
-                <Button type="gray">Gray</Button>
-            </div>
+            <main></main>
 
-            <div style={{width: '600px'}}>
-                <Input type="number"/>
-                <Input type="text"/>
-                <InputTag>
-                    <Input type="number"/>
-                    <Tag>KG</Tag>
-                </InputTag>
-            </div>
-
-            <div style={{width: '600px', paddingLeft: '2rem'}}>
-                <Card>
-                    <Header>Low</Header>
-                    <Info>Sitting all day, barely moving</Info>
-                </Card>
-                <Card selected={true}>
-                    <Header>Medium</Header>
-                    <Info>Sitting all day, barely moving</Info>
-                </Card>
-                <Card>
-                    <Header>Hight</Header>
-                    <Info>Sitting all day, barely moving</Info>
-                </Card>
-            </div>
+            <footer>
+                <div className={styles['buttons-container']}>
+                    <Button>Continue</Button>
+                </div>
+            </footer>
 
         </div>
     )
